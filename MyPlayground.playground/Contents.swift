1e4c4329fb91b961fcc61b20b0bb6128c652e7ed
Byeonghun Hwang <byeonghwang.com>
2017-08-09 15:22:43 +0900

    과일

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -16,3 +16,27 @@ age
 func getUserInfo() -> (String, Int, String){
     return ("고길동", 33, "010-1111-1111")
 }
+
+
+var arr1:Array<String>
+var arr2:[String]
+
+arr1 = Array()
+arr1 = Array<String>()
+
+arr2 = [String]()
+arr2 = []
+
+
+var arr3 = Array<String>()
+var arr4 = [String]()
+
+var fruits = ["사과", "딸기", "바나나"]
+
+print("과읠의 수는 \(fruits.count)개입니다.")
+fruits.insert("망고", at:0)
+fruits.append(contentsOf: ["메론", "복숭아"])
+
+for fruit in fruits {
+    print(fruit)
+}
